5e5ce45aa4a90991bc470da7f3c152343ff59702
principalkelvoe <you@example.com>
2022-01-04 02:28:28 +0300

    added transitions onload and on responsiveness

--- travelvue/src/views/Dashboard.vue
+++ travelvue/src/views/Dashboard.vue
@@ -1,39 +1,48 @@
-<template>    
-    <Asidebar />
+<template>
+  <Asidebar />
 </template>
 <script>
-
 import Asidebar from "@/components/Asidebar.vue";
 export default {
-    name:"Dashboard",
-    components:{
-        Asidebar
-    },
-    //  beforeCreate: function() {
-    //     document.body.className = 'dashboard';
-    // },
-//     beforeCreate() {
-//   this.$nextTick(() => {
-//     document.querySelector('body').className = 'dashboard'
-//   })
-// },
-// beforeDestroy() {
-//   document.querySelector('body').className = ''
-// },
-}
+  name: "Dashboard",
+  components: {
+    Asidebar,
+  },
+  //  beforeCreate: function() {
+  //     document.body.className = 'dashboard';
+  // },
+  //     beforeCreate() {
+  //   this.$nextTick(() => {
+  //     document.querySelector('body').className = 'dashboard'
+  //   })
+  // },
+  // beforeDestroy() {
+  //   document.querySelector('body').className = ''
+  // },
+};
 </script>
 <style>
-
 .dashboard {
-  background-color: #F9E2E3;
+  background-color: #f9e2e3;
   padding-left: 14rem;
+  transition: padding-left 1s;
+  animation: 1s ease-out 0s 1 slideInFromLeft;
 }
 
 @media (max-width: 1023px) {
- .dashboard{
+  .dashboard {
     padding-left: 0;
     overflow-x: hidden;
     width: 100vw;
+  }
+}
+
+@keyframes slideInFromLeft {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
   }
 }
 </style>
