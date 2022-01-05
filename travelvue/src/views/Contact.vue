1359e88fbc17d3a39476f40fa6ed7629234ed96e
principalkelvoe <you@example.com>
2022-01-05 23:20:48 +0300

    contact skeleton

---
+++ travelvue/src/views/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+<section>
+  <div class="contact">
+    <h1>This is an about page</h1>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+    name:'Contact'
+}
+</script>
