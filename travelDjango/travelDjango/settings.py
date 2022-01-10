c631b66696c30fb7fa361173fa560fc0d36d39d2
principalkelvoe <you@example.com>
2022-01-10 21:17:58 +0300

    worked on the models, ned to import pillow

--- travelDjango/travelDjango/settings.py
+++ travelDjango/travelDjango/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+MEDIA_URL ='/media/' #where to put uploaded image
+MEDIA_ROOT =BASE_DIR / 'media/'
--- travelDjango/travelDjango/urls.py
+++ travelDjango/travelDjango/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1', include('djoser.urls')),
     path('api/v1', include('djoser.urls.authtoken')),
-]
+] + static(settings.MEDIA_URL, document_root = settings. MEDIA_ROOT)
