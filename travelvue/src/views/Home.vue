b1c97ceaf62fe915f8825d124a92ff99fda481a0
principalkelvoe <you@example.com>
2022-01-11 17:06:46 +0300

    working on tips

--- travelvue/src/views/Home.vue
+++ travelvue/src/views/Home.vue
@@ -1,247 +1,308 @@
 <template>
   <div class="home section">
     <!--header corousel-->
-    <Fullwidth/>
+    <Fullwidth />
 
     <!--Top destination-->
     <section class="top_destination section hero">
       <div class="hero-body">
-      <div class="columns is-multiline">
-        <!--Top destination title-->
-        <div class="title column is-12">
-          <h2 class="is-size-5 has-text-centered">
-            Our most amazing place to explore
-          </h2>
-          <div class="line">
-            <h1 class="is-size-2 has-text-centered has-text-weight-bold">TOP DESTINATION</h1>
-          </div>
-        </div>
-
-        <!--Top destination top_shortcuts-->
-        <div class="columns is multiline">
-          <div class="column is-6">
-            <div class="top_shortcuts box">
-              <div class="relax shortcuts">
-                <a href="#" class="icon"><i class="fas fa-cocktail"></i> </a>
-                <p>Relax</p>
-              </div>
-
-              <div class="beach shortcuts">
-                <a href="#" class="icon"
-                  ><i class="fas fa-umbrella-beach"></i>
-                </a>
-                <p>Beach</p>
-              </div>
-
-              <div class="Historical shortcuts">
-                <a href="#" class="icon"><i class="fas fa-camera"></i></a>
-                <p>Historical</p>
-              </div>
-
-              <div class="cultural shortcuts">
-                <a href="#" class="icon"><i class="fas fa-monument"></i> </a>
-                <p>Cultural</p>
-              </div>
+        <div class="columns is-multiline">
+          <!--Top destination title-->
+          <div class="title column is-12">
+            <h2 class="is-size-5 has-text-centered">
+              Our most amazing place to explore
+            </h2>
+            <div class="line">
+              <h1 class="is-size-2 has-text-centered has-text-weight-bold">
+                TOP DESTINATION
+              </h1>
             </div>
           </div>
 
-          <div class="packages column is-6 has-text-centered">
-            <p class="speciality">
-              Our specialty is the ability to design unique tours and safaris to
-              fit every type of tourist, from the budget to the luxury providing
-              a unique opportunity to combine bush and beach adventures in
-              hidden treasures of Pokot, Kenya, East Africa's spectacular
-              sceneries and the World.
-            </p>
-            <button class="button mt-4">See all Packages</button>
+          <!--Top destination top_shortcuts-->
+          <div class="columns is multiline">
+            <div class="column is-6">
+              <div class="top_shortcuts box">
+                <div class="relax shortcuts">
+                  <a href="#" class="icon"><i class="fas fa-cocktail"></i> </a>
+                  <p>Relax</p>
+                </div>
+
+                <div class="beach shortcuts">
+                  <a href="#" class="icon"
+                    ><i class="fas fa-umbrella-beach"></i>
+                  </a>
+                  <p>Beach</p>
+                </div>
+
+                <div class="Historical shortcuts">
+                  <a href="#" class="icon"><i class="fas fa-camera"></i></a>
+                  <p>Historical</p>
+                </div>
+
+                <div class="cultural shortcuts">
+                  <a href="#" class="icon"><i class="fas fa-monument"></i> </a>
+                  <p>Cultural</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="packages column is-6 has-text-centered">
+              <p class="speciality">
+                Our specialty is the ability to design unique tours and safaris
+                to fit every type of tourist, from the budget to the luxury
+                providing a unique opportunity to combine bush and beach
+                adventures in hidden treasures of Pokot, Kenya, East Africa's
+                spectacular sceneries and the World.
+              </p>
+              <button class="button mt-4">See all Packages</button>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </section>
 
     <!--Our Tour Package-->
     <section class="tour_packages section hero">
       <div class="hero-body">
-      <div class="columns is-multiline">
-        <!--Our Tour Package title-->
-        <div class="title column is-12">
-          <h2 class="is-size-5 has-text-centered">see amazing capture</h2>
-          <div class="line">
-            <h1 class="is-size-2 has-text-centered has-text-weight-bold">OUR TOUR PACKAGE</h1>
+        <div class="columns is-multiline">
+          <!--Our Tour Package title-->
+          <div class="title column is-12">
+            <h2 class="is-size-5 has-text-centered">see amazing capture</h2>
+            <div class="line">
+              <h1 class="is-size-2 has-text-centered has-text-weight-bold">
+                OUR TOUR PACKAGE
+              </h1>
+            </div>
           </div>
         </div>
-      </div>
 
-      <!--packages-->
-      <div class="columns is-multiline">
-        <div class="column is-4 is-dark">
-          <div class="image p-4">
-            <img src="../assets/footer_img.jpg" alt="" />
+        <!--packages-->
+        <div class="columns is-multiline packages">
+          <div class="column is-4 is-dark">
+            <figure class="image is-5by4 m-2">
+              <img src="../assets/footer_img.jpg" alt="" />
+            </figure>
+            <figure class="image is-5by4 m-2">
+              <img src="../assets/footer_img.jpg" alt="" />
+            </figure>
           </div>
-          <div class="image p-4">
-            <img src="../assets/footer_img.jpg" alt="" />
+          <div class="column is-4 tour_form box">
+            <div></div>
+          </div>
+          <div class="column is-4">
+            <figure class="image is-5by4 m-2">
+              <img src="../assets/footer_img.jpg" alt="" />
+            </figure>
+            <figure class="image is-5by4 m-2">
+              <img src="../assets/footer_img.jpg" alt="" />
+            </figure>
           </div>
         </div>
-        <div class="column is-4 box">
-          <div class="tour_form"></div>
-        </div>
-        <div class="column is-4">
-          <div class="image p-4">
-            <img src="../assets/footer_img.jpg" alt="" />
-          </div>
-          <div class="image p-4">
-            <img src="../assets/footer_img.jpg" alt="" />
-          </div>
-        </div>
-      </div>
       </div>
     </section>
 
     <!--Our Travel Tips-->
     <section class="travel_tips section hero">
       <div class="hero-body">
-      <div class="columns is-multiline">
-        <!--Our Travel Tips title-->
-        <div class="title column is-12">
-          <h2 class="is-size-5 has-text-centered">Go for Categories</h2>
-          <div class="line">
-            <h1 class="is-size-2 has-text-centered has-text-weight-bold">TRAVEL TIPS</h1>
+        <div class="columns is-multiline">
+          <!--Our Travel Tips title-->
+          <div class="title column is-12">
+            <h2 class="is-size-5 has-text-centered">Go for Categories</h2>
+            <div class="line">
+              <h1 class="is-size-2 has-text-centered has-text-weight-bold">
+                TRAVEL TIPS
+              </h1>
+            </div>
           </div>
         </div>
 
         <!--bg-->
-        <div class="travel_tips_bg">
-          <div class="columns is-multiline">
-            <div class="column is-12">
-              <div class="column is-6"></div>
-              <div class="column is-3"></div>
-            </div>              <div class="column is-3"></div>
 
+        <div class="travel_tips_bg columns is-multiline">
+          <div class="column is-4 m-3 has-text-centered">
+            <div class="company_tips">
+            <h2 class="subtitle">Culture of Pokot</h2>
+            <p>
+              We seek to maximize the positive impact of our trips on the local
+              communities we visit and eliminate potential negative effects
+            </p>
+            </div>
+          </div>
+          
+          <div class="column is-3 m-3 pt-6">
+            <figure class="image is-4by5">
+              <img src="../assets/fullheight/sooz-eRSB_q75Xm4-unsplash.jpg" alt="" />
+              
+            </figure>
+            <p class="trip_description has-text-centered">
+                Pokot tours is a national award winning community based rural
+                tourism company.<br> That aims to unveil Kenyan, for visitors who
+                would like to explore the Kenyan culture.
+              </p>
+          </div>
+
+          <div class="column is-3 m-3 pt-6 mb-6">
+            <figure class="image is-4by5">
+              <img src="../assets/fullheight/foods.jpg" alt="" />
+            </figure>
+            <p class="trip_description has-text-centered">
+                Pokot tours is a national award winning community based rural
+                tourism company.<br> That aims to unveil Kenyan, for visitors who
+                would like to explore the Kenyan culture.
+              </p>
           </div>
         </div>
-      </div>
       </div>
     </section>
 
     <!--Our services-->
     <section class="services section hero">
       <div class="hero-body">
-      <div class="columns is-multiline">
-        <!--Our services title-->
-        <div class="title column is-12">
-          <h2 class="is-size-5 has-text-centered">we fulfil your need</h2>
-          <div class="line">
-            <h1 class="is-size-2 has-text-centered has-text-weight-bold">SERVICES</h1>
-          </div>
-        </div>
-      </div>
-
-      <!-- first column -->
-      <div class="columns is-multiline is-gapless services">
-        <div class="column is-6">
-          <div class="is-bordered">
-            <div class="column is-6">
-              <figure class="image is-square">
-                <img class="is-rounded" src="../assets/footer_img.jpg" alt="" />
-              </figure>
-            </div>
-          </div>
-        </div>
-        <div class="column is-6">
-          <div class="is-bordered">
-            <div class="column is-6 is-pulled-right">
-              <figure class="image is-square">
-                <img class="is-rounded" src="../assets/footer_img.jpg" alt="" />
-              </figure>
+        <div class="columns is-multiline">
+          <!--Our services title-->
+          <div class="title column is-12">
+            <h2 class="is-size-5 has-text-centered">we fulfil your need</h2>
+            <div class="line">
+              <h1 class="is-size-2 has-text-centered has-text-weight-bold">
+                SERVICES
+              </h1>
             </div>
           </div>
         </div>
 
-        <!-- second column -->
-        <div class="column is-6 second_column">
-          <div class="is-bordered">
-            <div class="column is-6 is-pulled-right">
-              <figure class="image is-square">
-                <img class="is-rounded" src="../assets/footer_img.jpg" alt="" />
-              </figure>
+        <!-- first column -->
+        <div class="columns is-multiline is-gapless services">
+          <div class="column is-6">
+            <div class="is-bordered">
+              <div class="column is-6">
+                <figure class="image is-square">
+                  <img
+                    class="is-rounded"
+                    src="../assets/footer_img.jpg"
+                    alt=""
+                  />
+                </figure>
+              </div>
             </div>
           </div>
-        </div>
+          <div class="column is-6">
+            <div class="is-bordered">
+              <div class="column is-6 is-pulled-right">
+                <figure class="image is-square">
+                  <img
+                    class="is-rounded"
+                    src="../assets/footer_img.jpg"
+                    alt=""
+                  />
+                </figure>
+              </div>
+            </div>
+          </div>
 
-        <div class="column is-6 second_column">
-          <div class="is-bordered">
-            <div class="column is-6">
-              <figure class="image is-square">
-                <img class="is-rounded" src="../assets/footer_img.jpg" alt="" />
-              </figure>
+          <!-- second column -->
+          <div class="column is-6 second_column">
+            <div class="is-bordered">
+              <div class="column is-6 is-pulled-right">
+                <figure class="image is-square">
+                  <img
+                    class="is-rounded"
+                    src="../assets/footer_img.jpg"
+                    alt=""
+                  />
+                </figure>
+              </div>
             </div>
           </div>
-        </div>
 
-        <!-- third column -->
-        <div class="column is-6">
-          <div class="is-bordered">
-            <div class="column is-6">
-              <figure class="image is-square">
-                <img class="is-rounded" src="../assets/footer_img.jpg" alt="" />
-              </figure>
+          <div class="column is-6 second_column">
+            <div class="is-bordered">
+              <div class="column is-6">
+                <figure class="image is-square">
+                  <img
+                    class="is-rounded"
+                    src="../assets/footer_img.jpg"
+                    alt=""
+                  />
+                </figure>
+              </div>
+            </div>
+          </div>
+
+          <!-- third column -->
+          <div class="column is-6">
+            <div class="is-bordered">
+              <div class="column is-6">
+                <figure class="image is-square">
+                  <img
+                    class="is-rounded"
+                    src="../assets/footer_img.jpg"
+                    alt=""
+                  />
+                </figure>
+              </div>
+            </div>
+          </div>
+          <div class="column is-6">
+            <div class="is-right">
+              <div class="column is-6 is-pulled-right">
+                <figure class="image is-square">
+                  <img
+                    class="is-rounded"
+                    src="../assets/footer_img.jpg"
+                    alt=""
+                  />
+                </figure>
+              </div>
             </div>
           </div>
         </div>
-        <div class="column is-6">
-          <div class="is-right">
-            <div class="column is-6 is-pulled-right">
-              <figure class="image is-square">
-                <img class="is-rounded" src="../assets/footer_img.jpg" alt="" />
-              </figure>
-            </div>
-          </div>
-        </div>
-      </div>
       </div>
     </section>
 
     <!-- About Us -->
     <section class="about_us section hero">
       <div class="hero-body">
-      <div class="columns is-multiline">
-        <!--About Us title-->
-        <div class="title column is-12">
-          <h2 class="is-size-5 has-text-centered">Our featured story</h2>
-          <div class="line">
-            <h1 class="is-size-2 has-text-centered has-text-weight-bold">ABOUT US</h1>
+        <div class="columns is-multiline">
+          <!--About Us title-->
+          <div class="title column is-12">
+            <h2 class="is-size-5 has-text-centered">Our featured story</h2>
+            <div class="line">
+              <h1 class="is-size-2 has-text-centered has-text-weight-bold">
+                ABOUT US
+              </h1>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="columns is-multiline is-centered">
-        <div class="column is-5">
-          <h2 class="is-size-4 has-text-weight-semibold">We since from 2011</h2>
-          <span class="has-text-left"
-            >Pokot Tours and Travel Ltd is a well-established Company in west
-            Pokot County handling inbound and outbound holidays for domestic
-            tourists and international tourists from all over the world.</span
-          >
-          <h2 class="is-size-4  mt-5 has-text-weight-semibold">Our Success</h2>
-          <span class="has-text-left"
-            >Our specialty is the ability to design unique tours and safaris to
-            fit every type of tourist, from the budget to the luxury providing a
-            unique opportunity to combine bush and beach adventures in hidden
-            treasures of Pokot, Kenya, East Africa's spectacular sceneries and
-            the World.</span
-          >
+        <div class="columns is-multiline is-centered">
+          <div class="column is-5">
+            <h2 class="is-size-4 has-text-weight-semibold">
+              We since from 2011
+            </h2>
+            <span class="has-text-left"
+              >Pokot Tours and Travel Ltd is a well-established Company in west
+              Pokot County handling inbound and outbound holidays for domestic
+              tourists and international tourists from all over the world.</span
+            >
+            <h2 class="is-size-4 mt-5 has-text-weight-semibold">Our Success</h2>
+            <span class="has-text-left"
+              >Our specialty is the ability to design unique tours and safaris
+              to fit every type of tourist, from the budget to the luxury
+              providing a unique opportunity to combine bush and beach
+              adventures in hidden treasures of Pokot, Kenya, East Africa's
+              spectacular sceneries and the World.</span
+            >
 
-          <hr class="hr">
-          
+            <hr class="hr" />
 
-          <button class="button mt-4">Read More</button>
+            <button class="button mt-4">Read More</button>
+          </div>
+          <div class="column is-5">
+            <figure class="image is-4by5">
+              <img src="../assets/footer_img.jpg" alt="" />
+            </figure>
+          </div>
         </div>
-        <div class="column is-5">
-          <figure class="image is-4by5">
-            <img src="../assets/footer_img.jpg" alt="" />
-          </figure>
-        </div>
-      </div>
       </div>
     </section>
 
@@ -335,14 +396,14 @@
 <script>
 //connect to the backend
 import axios from "axios";
-import Fullwidth from "../components/Fullwidth_Carousel"
+import Fullwidth from "../components/Fullwidth_Carousel";
 export default {
   name: "Home",
   data() {
     return {};
   },
   components: {
-    Fullwidth
+    Fullwidth,
   },
   setup() {
     const carouselSlides = [
@@ -361,8 +422,6 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-
-
 //top destination
 .top_shortcuts {
   display: flex;
@@ -413,6 +472,7 @@ export default {
 .services {
   position: relative;
 }
+
 .column.is-6.second_column {
   top: 25%;
   /* border: 1px solid black; */
@@ -420,10 +480,44 @@ export default {
   position: absolute;
 }
 
+.tour_form {
+  background: #660746;
+  border-radius: 6px;
+  box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 10%),
+    0 0px 0 1px rgb(10 10 10 / 2%);
+  color: #f9e2e3;
+}
 
+//travel tips
+.travel_tips_bg {
+  background: #660746;
+  color: #f9e2e3;
+  justify-content: center;
+  // align-items: center;
+  border-radius: 10px;
+  box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 10%),
+    0 0px 0 1px rgb(10 10 10 / 2%);
+  height: 90vh;
+  h2.subtitle {
+    color: #f9e2e3;
+    font-size: 1.4em;
+    font-weight: bolder;
+    
+  }
+  .company_tips{
+      border: #f9e2e3 solid;
+      margin-top: 20%;
+      font-size: 1.2em;
+      padding: .4em;
+    }
+  .trip_description{
+    background: #f9e2e3;
+    color: #000000;
+  }
+}
 
 //about us
-.hr{
+.hr {
   background-color: #660746;
 }
 
