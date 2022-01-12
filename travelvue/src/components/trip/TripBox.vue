ce8681f3338e6816ff6ba32cf3912e04caa0faf0
principalkelvoe <you@example.com>
2022-01-12 09:23:07 +0300

    created tripbox comonent

--- travelvue/src/components/trip/TripBox.vue
+++ travelvue/src/components/trip/TripBox.vue
@@ -4,6 +4,19 @@
             <figure class="image mb-4">
                 <img v-bind:src="trip.get_thumbnail">
             </figure>
+            <h3 class="is-size-4">{{trip.name}}</h3>
+            <p class="is-size-6 has-text-grey">${{trip.price}}</p>
+            <!-- <router-link v:bind:to="trip.get_absolute_url" class="button is-dark mt-4">View details</router-link> -->
+
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    name:'TripBox',
+    props:{
+        trip:Object
+    }
+}
+</script>
