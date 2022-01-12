71cb8c7782ece2e0cd705aafbbaeaedf638a3d34
principalkelvoe <you@example.com>
2022-01-12 14:38:14 +0300

    styles for trip images

--- travelvue/src/components/trip/TripBox.vue
+++ travelvue/src/components/trip/TripBox.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="column is-3">
         <div class="box">
-            <figure class="image mb-4">
+            <figure class="image mb-3 is-4by5">
                 <img v-bind:src="trip.get_thumbnail">
             </figure>
-            <h3 class="is-size-4">{{trip.name}}</h3>
+            <h3 class="is-size-4">{{trip.name}} </h3>
             <p class="is-size-6 has-text-grey">${{trip.price}}</p>
             <!-- <router-link v:bind:to="trip.get_absolute_url" class="button is-dark mt-4">View details</router-link> -->
 
@@ -20,3 +20,25 @@ export default {
     }
 }
 </script>
+<style lang="scss" scoped>
+.box{
+    background-color:transparent;
+    box-shadow: none;
+}
+.box:hover{
+// box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 10%), 0 0px 0 1px rgb(10 10 10 / 2%);
+img{
+box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 100%), 0 0px 0 1px rgb(10 10 10 / 100%);
+    
+}
+
+}
+
+.image img{
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+    border-radius: 10%;
+    transition: border-radius 1s;
+}
+</style>
