20597541b4711a11d6d650dcf7ad045f57947a51
principalkelvoe <you@example.com>
2022-01-12 08:28:45 +0300

    creating component for trips

---
+++ travelvue/src/components/trip/TripBox.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="column is-3">
+        <div class="box">
+            <figure class="image mb-4">
+                <img v-bind:src="trip.get_thumbnail">
+            </figure>
+        </div>
+    </div>
+</template>
