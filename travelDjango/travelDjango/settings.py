1a336fb4e94d047e066810ee34a1216872ecc906
principalkelvoe <you@example.com>
2022-01-12 14:40:29 +0300

    small edits to call te backend

--- travelDjango/travelDjango/settings.py
+++ travelDjango/travelDjango/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS=[
+    'http://localhost:8080',
+]
+
 
 # Application definition
 
--- travelDjango/travelDjango/urls.py
+++ travelDjango/travelDjango/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1', include('djoser.urls')),
-    path('api/v1', include('djoser.urls.authtoken')),
-    path('api/v1',include('trip.urls')),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/',include('trip.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings. MEDIA_ROOT)
