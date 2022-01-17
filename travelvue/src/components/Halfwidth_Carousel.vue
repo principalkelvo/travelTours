81f68a02e6a6903e2199d7a036fd56b17b854d06
principalkelvoe <you@example.com>
2022-01-17 22:36:39 +0300

    created half width caourasel working

---
+++ travelvue/src/components/Halfwidth_Carousel.vue
@@ -0,0 +1,107 @@
+<template>
+  <section class="mb-2 p-0">
+    <!--carousel-->
+    <div>
+      <Carousel
+        :navigation="false"
+        :pagination="false"
+        :startAutoPlay="true"
+        :timeout="10000"
+        class="carousel"
+        v-slot="{ currentSlide }"
+      >
+        <!--the captions-->
+        <div class="has-text-left captions">
+          <div class="background-captions">
+            <p class="title mb-6 is-uppercase">Welcome to Pokot</p>
+            <p class="subtitle">Kenya spectacular sceneries</p>
+          </div>
+        </div>
+
+        <!--background images-->
+        <Slide v-for="(slide, index) in carouselSlides" :key="index">
+          <div
+            v-show="currentSlide === index + 1"
+            class="slide-info image is-fullwidth"
+          >
+            <img
+              :src="require(`../assets/${slide}.jpg`)"
+              alt=""
+              class=""
+            />
+          </div>
+        </Slide>
+      </Carousel>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+import Carousel from "../components/Carousel.vue";
+import Slide from "../components/Slide.vue";
+
+export default {
+  name: "Fullwidth",
+  data() {},
+  components: {
+    Carousel,
+    Slide,
+  },
+  setup() {
+    const carouselSlides = [
+      "bg- 1",
+      "bg- 2",
+      "bg- 3",
+      "bg- 4",
+      "bg- 5",
+      "bg- 6",
+      "bg- 7",
+    ];
+    return { carouselSlides };
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+.carousel {
+  position: relative;
+  height: 20vh;
+  display: flex;
+  // justify-content: center;
+  .captions {
+    position: absolute;
+    z-index: 1;
+    width: 50%;
+    bottom: 20%;
+    left: 3%;
+    p {
+      color: rgb(255, 255, 255);
+      font-weight: 900;
+    }
+  }
+  //   .background-captions {
+  //     background: rgba(0, 0, 0, 0.5);
+  //     transition: 2s;
+  //     padding-top: 10px;
+  //     padding-bottom: 20px;
+  //     border-radius: 20px;
+  //   }
+
+  .slide-info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    max-height: 100%;
+    height: 100%;
+    img {
+      border-radius: 10px;
+      min-width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
