a4bf286b9fd18a0292b2b4077ee531d27881f2bd
principalkelvoe <you@example.com>
2022-01-21 00:41:13 +0300

    url for category details

--- travelDjango/trip/urls.py
+++ travelDjango/trip/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('latest-trips/',views.LatestTripList.as_view()),
     path('popular-trips/',views.PopularTripList.as_view()),
     path('trips/<slug:category_slug>/<slug:trip_slug>/',views.TripDetail.as_view()),
+    path('trips/<slug:category_slug>/',views.CategoryDetail.as_view()),
 
 ]
