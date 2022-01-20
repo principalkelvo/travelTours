87ae02d2f507b188da0763d9969f3eef14843f09
principalkelvoe <you@example.com>
2022-01-21 01:29:24 +0300

    added items from backend

--- travelvue/src/views/Dashboard.vue
+++ travelvue/src/views/Dashboard.vue
@@ -1,18 +1,87 @@
 <template>
   <section class="section">
+    <!-- side bar  -->
   <Asidebar />
+  <!-- carousel -->
   <Halfwidth />
 
+  <div class="columns is-multiline hero-body">
+      <div class="column is-12">
+        <h2 class="is-size-3 has-text-weight-bold has-text-left">
+          Featured Offers <b style="color:red">❤</b>
+        </h2>
+      </div>
+
+      <TripBox
+        v-for="trip in latestTrips"
+        v-bind:key="trip.id"
+        v-bind:trip="trip"
+      />
+    </div>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-3 has-text-weight-bold has-text-left">
+          Popular Destinations ⭐
+        </h2>
+      </div>
+
+      <TripBox
+        v-for="trip in popularTrips"
+        v-bind:key="trip.id"
+        v-bind:trip="trip"
+      />
+    </div>
   </section>
 </template>
 <script>
+import axios from "axios";
+
+// components
 import Asidebar from "@/components/Asidebar.vue";
 import Halfwidth from "@/components/Halfwidth_Carousel.vue";
+import TripBox from "@/components/trip/TripBox";
+
 export default {
   name: "Dashboard",
+  data() {
+    return {
+      latestTrips: [],
+      popularTrips: [],
+    };
+  },
   components: {
     Asidebar,
     Halfwidth,
+    TripBox,
+  },
+  
+  mounted() {
+    this.getLatestTrips() 
+    this.getPopularTrips();
+  },
+  methods: {
+    getLatestTrips() {
+      axios
+        .get("/api/v1/latest-trips/")
+        .then((response) => {
+          this.latestTrips = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getPopularTrips() {
+      axios
+        .get("/api/v1/popular-trips/")
+        .then((response) => {
+          this.popularTrips = response.data;
+          console.log(this.popularTrips)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
   //  beforeCreate: function() {
   //     document.body.className = 'dashboard';
@@ -51,5 +120,10 @@ export default {
   100% {
     transform: translateX(0);
   }
+}
+</style>
+<style scoped>
+h2{
+  color: black;
 }
 </style>
