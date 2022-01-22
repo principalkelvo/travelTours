7aa9fe5eb12c0694c3e5eb35343c8d4808cdd0b1
principalkelvoe <you@example.com>
2022-01-22 11:18:54 +0300

    urls change

--- travelDjango/trip/urls.py
+++ travelDjango/trip/urls.py
@@ -5,7 +5,7 @@ from trip import views
 urlpatterns = [
     path('latest-trips/',views.LatestTripList.as_view()),
     path('popular-trips/',views.PopularTripList.as_view()),
-    path('trips/search/',views.search),
+    path('trips/search',views.search),
     path('trips/<slug:category_slug>/<slug:trip_slug>/',views.TripDetail.as_view()),
     path('trips/<slug:category_slug>/',views.CategoryDetail.as_view()),
 
