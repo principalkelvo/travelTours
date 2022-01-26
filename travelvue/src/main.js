bf056eebdbb053258426a0cb3f5f5dd6f3801ea5
principalkelvoe <you@example.com>
2022-01-26 15:47:23 +0300

    added comments

--- travelvue/src/main.js
+++ travelvue/src/main.js
@@ -1,7 +1,16 @@
+// styles
+import '@/scss/main.scss'
+
+// core
 import { createApp } from 'vue'
+
+/* Vue. Main component */
 import App from './App.vue'
+
+/* Router & Store */
 import router from './router'
 import store from './store'
+
 import axios from 'axios'
 
 axios.defaults.baseURL= 'http://127.0.0.1:8000'
