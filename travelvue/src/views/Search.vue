08cfa506905896db255f7f9dd99f3fa5a0334ce5
principalkelvoe <you@example.com>
2022-01-28 22:48:52 +0300

    working change in url

--- travelvue/src/views/Search.vue
+++ travelvue/src/views/Search.vue
@@ -1,9 +1,35 @@
 <template>
     <div class="page-search">
-        <div class="columns is-multiline">
-            <div class="column is-12">
+        <div class="utility columns is-multiline">
+            <div class="column is-4">
                 <h1 class="title">Search</h1>
                 <h2 class="is-size-5 has-text-grey">Search term: " {{query}} "</h2>
+            </div>
+            
+            <!-- for filtering based on price  -->
+            <div class="column is-4">
+                <select name="price" id="price-list" onchange="location = finalurl2()">
+                    <option value="" id="price-placeholder" disabled selected hidden>Price</option>
+                    <option id="pr500" value="500">less than 500</option>
+                    <option id="pr1000" value="1000">less than 1000</option>
+                    <option id="pr2000" value="2000">less than 2000</option>
+                    <option id="pr5000" value="5000">less than 5000</option>
+                    <option id="pr10000" value="10000">less than 10000</option>
+
+                </select>
+            </div>
+
+            <!-- for making sorting functionality -->
+            <div class="column is-4">
+                <select name="sort" id="sort-list" @change="finalurl()">
+                    <option value="" id="price-placeholder" disabled selected hidden>Sort</option>
+                    <option id="price" value="price">Price: Low to High</option>
+                    <option id="price" value="-price">Price: High to Low</option>
+                    <option id="trip_name" value="name">Title</option>
+                    <option id="-date_added" value="-date_added">What's New</option>
+
+                </select>
+                
             </div>
             <TripBox
                 class="column is-3"
@@ -19,6 +45,9 @@
 import axios from 'axios'
 import TripBox from "@/components/trip/TripBox";
 
+
+
+
 export default {
     name:'Search',
     components:{
@@ -27,11 +56,13 @@ export default {
     data(){
         return{
             trips:[],
-            query:''
+            query:'',
         }
     },
     mounted(){
         document.title='Search | Trip'
+
+        //all search
         let uri= window.location.search.substring(1)
         let params = new URLSearchParams(uri)
         if(params.get('query')){
@@ -51,8 +82,22 @@ export default {
                     console.log(error)
                 })
 
+        },
+        finalurl(){
+            var url = new URL(window.location.href);
+            console.log(url);
+            var search_params = url.searchParams
+            search_params.set('ordering',document.getElementById('sort-list').value);
+            url.search= search_params.toString();
+            var new_url = url.toString();
+            window.location = new_url
         }
     }
 
 }
 </script>
+<style scoped>
+h1::before{
+    content: '';
+}
+</style>
